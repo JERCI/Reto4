8ee540546c82a2490926f71f3cbeb6b5f10fce4e
JERCI <110736693+JERCI@users.noreply.github.com>
2022-08-06 12:17:13 -0500

    Update clientes.sql

--- clientes.sql
+++ clientes.sql
@@ -7,11 +7,11 @@ create table clientes (
     contraseña int,
     fecha_nacimiento date
 );
-insert into clientes values ("lucky", "Pedro", "Perez", "luckyperez@gmail.com", "3006074546", 81726349, "1983-10-24" );
-insert into clientes values ("malopez", "Maria", "Lopez", "maria@email.com", "3054667872", 12344567, "2004-03-03");
-insert into clientes values ("diva", "Ana", "Diaz", "anadiaz@gmail.com", "3206479833", 12345679, "1982-08-08");
-insert into clientes values ("dreamer", "Luis", "Rojas", "luisrojas@hotmail.com", "3044504466", 8791634, "1990-05-26");
-insert into clientes values ("ninja", "Andres", "Cruz", "nijaac@gmail.com", "3125408796", 96124215, "1991-06-16");
-insert into clientes values ("neon", "Nelson", "Ruiz", "nelsoneon@hotmail.com", "3156789345", 00133246, "1992-07-26");
-insert into clientes values ("rose", "Claudia", "Mendez", "rosemendez@gmail.com", "3117648907", 88746561, "2003-03-18");
-insert into clientes values ("green", "Jorge", "Rodriguez","rodriguezjorge@gmail.com", "3107863457", 11132445, "2002-08-05");
+insert into clientes values ("lucky", "Pedro", "Perez", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", 81726349, "1983-11-28" );
+insert into clientes values ("malopez", "Maria", "Lopez", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI , "FECHA DE NACIMIENTO");
+insert into clientes values ("diva", "Ana", "Diaz", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
+insert into clientes values ("dreamer", "Luis", "Rojas", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
+insert into clientes values ("ninja", "Andres", "Cruz", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
+insert into clientes values ("neon", "Nelson", "Ruiz", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
+insert into clientes values ("rose", "Claudia", "Mendez", " INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
+insert into clientes values ("green", "Jorge", "Rodriguez"," INVENTAR CORREO ", " INVENTAR N° CELULAR ", CONTRASEÑA 8 DIGI, "FECHA DE NACIMIENTO");
